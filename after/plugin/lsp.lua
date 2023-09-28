53441ec621dbd132780d52feb7cfd6f44005ed05
Robert Perez <rickymcmuffins@gmail.com>
2023-09-28 14:38:56 -0400

    added comment plugin

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,26 +1,25 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
-    lsp_zero.default_setup,
-  },
+	ensure_installed = {},
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-  })
+	mapping = cmp.mapping.preset.insert({
+		['<Tab>'] = cmp_action.tab_complete(),
+		['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+	})
 })
-
