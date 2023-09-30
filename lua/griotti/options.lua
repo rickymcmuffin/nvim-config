75c46ba4e8fbd06498b9cc94c751d1d995685378
Robert Perez <rickymcmuffins@gmail.com>
2023-09-30 17:35:31 -0400

    added reamps and options

--- lua/griotti/options.lua
+++ lua/griotti/options.lua
@@ -1,5 +1,22 @@
 vim.opt.splitbelow = true
+vim.opt.splitright = true
 vim.opt.shell= 'powershell.exe'
---vim.opt.shellcmdflag=-'command'
---vim.opt.shellquote=\"
---vim.opt.shellxquote=
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
--- lua/griotti/remap.lua
+++ lua/griotti/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- switch between windows
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
@@ -10,4 +12,20 @@ vim.keymap.set("n", "<C-t>", ":split<cr>:term<cr>i")
 
 -- exits terminal
 vim.keymap.set("t", "<C-t>", "<C-\\><C-n>:q<cr>")
+
+-- moves highlighted text up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- cursor in the middle when scrolling
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+
+
 
