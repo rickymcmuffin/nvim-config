c163b5d6fb045592f560b9e59b5472d5cd53bd37
Robert Perez <rickymcmuffins@gmail.com>
2024-03-06 11:48:31 -0500

    switched to tokyonight

--- lua/griotti/lazy.lua
+++ lua/griotti/lazy.lua
@@ -13,16 +13,23 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup(
 	{
-		{
-			"maxmx03/solarized.nvim",
-			lazy = false, -- make sure we load this during startup if it is your main colorscheme
-			priority = 1000, -- make sure to load this before all the other start plugins
-			config = function()
-				-- load the colorscheme here
-                vim.o.background = 'dark'
-				vim.cmd([[colorscheme solarized]])
-			end,
-		},
+		-- {
+		-- 	"maxmx03/solarized.nvim",
+		-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		-- 	priority = 1000, -- make sure to load this before all the other start plugins
+		--
+		-- 	config = function()
+		-- 		-- load the colorscheme here
+  --               vim.o.background = 'dark'
+		-- 		vim.cmd([[colorscheme solarized]])
+		-- 	end,
+		-- },
+        {
+            "folke/tokyonight.nvim",
+            lazy = false,
+            priority = 1000,
+            opts = {},
+        },
 
 
 		{
