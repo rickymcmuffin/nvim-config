08d1bb6fb672e560805677ca22c5e17085894adb
Robert Perez <rickymcmuffins@gmail.com>
2025-07-07 14:10:16 -0400

    added deleting buffers

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,3 +3,19 @@ vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      n = {
+    	  ['<c-d>'] = require('telescope.actions').delete_buffer
+      }, -- n
+      i = {
+        ['<c-d>'] = require('telescope.actions').delete_buffer
+      } -- i
+    } -- mappings
+  }, -- defaults
+...
+} -- telescope setup
