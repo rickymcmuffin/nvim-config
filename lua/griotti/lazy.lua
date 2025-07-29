b1ef867b1589f9812d6e9f3b099e75e185f1bf70
Robert Perez <rickymcmuffins@gmail.com>
2025-07-29 15:12:07 -0500

    removed lsp zero

--- lua/griotti/lazy.lua
+++ lua/griotti/lazy.lua
@@ -45,7 +45,7 @@ require("lazy").setup(
 		{ 'williamboman/mason.nvim' },
 		{ 'williamboman/mason-lspconfig.nvim' },
 
-		{ 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
+		-- { 'VonHeikemen/lsp-zero.nvim',        branch = 'v3.x' },
 		{ 'neovim/nvim-lspconfig' },
 		{ 'hrsh7th/cmp-nvim-lsp' },
 		{ 'hrsh7th/nvim-cmp' },
